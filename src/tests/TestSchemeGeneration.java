6311b77b75928cc05e9737045fd8db74c5f725d1
Diana Malabard <diana.malabard@gmail.com>
2015-02-01 12:43:17 +0100

    test update

--- src/tests/TestSchemeGeneration.java
+++ src/tests/TestSchemeGeneration.java
@@ -3,20 +3,22 @@ package tests;
 import javax.naming.directory.InvalidAttributeValueException;
 
 import typeGenerators.GenerateurTypesV1;
+import typeGenerators.GenerateurTypesV3;
 import binTree.termes.Terme;
 import binTree.types.Type;
 
+@SuppressWarnings("unused")
 public class TestSchemeGeneration {
 
 	public static void main(String[] args) {
 		
-		GenerateurTypesV1 generator;
+		GenerateurTypesV3 generator;
 		try {
-			generator = new GenerateurTypesV1(0.24444);
+			generator = new GenerateurTypesV3(1/8.0-1e-8);
 			
 			Type t = generator.generate();
 			
-			Terme term = t.generateTermeV3(3);			
+			Terme term = t.generateTermeV3(15);			
 			
 			System.out.println(term.toScheme());
 		} catch (InvalidAttributeValueException e) {
